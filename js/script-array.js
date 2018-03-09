7b83ba54ca67ad9a682ae7b1a71ac3fc553160cd
lihen12 <henryli0@yahoo.com>
2018-03-09 03:59:16 -0800

    Added text area to include DOM manipulation of products

--- js/script-array.js
+++ js/script-array.js
@@ -4,6 +4,7 @@ var cart = []
 //global variable for prices of items
 var itemPrice = []
 
+var cartContainer = document.querySelector
 /* ***************************************************************************** */
 //function to update the cart array when onclick pases on parameter "item"
 function cartPush(item, price){
@@ -33,10 +34,36 @@ function cartPush(item, price){
   }
   
   console.log("Your total price in cart is $" + sum);
+  
+  // Add total number of products in cart next to cart icon
+  if(cart.length != 0) {
+    document.querySelector("#cartcount").innerHTML = ("  (" + cart.length + ")")
+  }
+  // Make the cart number disappear if there's nothing inside
+  else {
+    document.querySelector("#cartcount").innerHTML = ""
+  }
+
 }
 
 /* ***************************************************************************** */
 // tells us what items are in the "cart" array
+
 function cartInfo(){
   console.log("Here's what's in your cart: " + cart);
+  
+  var sum = itemPrice.reduce(add, 0);
+
+  function add(a, b){
+    return a + b;
+  }
+  
+  var allItems = ""
+  
+  for(var i = 0; i < cart.length; i++){
+    allItems += " " + cart[i] + "\n";
+  }
+  
+  document.querySelector("#cartinfo").innerHTML = "Your Items in Cart:\n" + allItems + "Total Price: " + sum;
+  
 }
